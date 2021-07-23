3f4f43a1b3935d35ac69b5dc487aeda5cf156266
mectx02 <mectx02@icloud.com>
2021-07-23 15:51:02 -0500

    Updated install script and added local desktop directory folder

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # Mostly just copy commands, so I'm okay with using /bin/sh
 
-# 
-# Variables
-# 
 
 # User identification for home folder
 user="$(id -u 1000 -n)"
@@ -12,22 +9,41 @@ user="$(id -u 1000 -n)"
 fontdir=/usr/share/fonts/
 homedir=/home/$user
 configdir=$homedir/.config/
+desktopdir=$homedir/.local/share/applications/
 
 
 # Check for root status
 [ "$UID" -eq 0 ] || exec sudo /bin/sh "$0" "$@"
 
-# Copy commands
+
+# Check for existence of sway, waybar, and alacritty config directories
+# If they don't exist, make them (and save some trouble later)
+[ ! -d "$configdir/sway" ] && mkdir -p $configdir/sway && echo "Made directory for sway config at $configdir/sway"
+[ ! -d "$configdir/waybar" ] && mkdir -p $configdir/waybar && echo "Made directory for waybar config at $configdir/waybar"
+[ ! -d "$configdir/alacritty" ] && mkdir -p $configdir/alacritty && echo "Made directory for alacritty config at $configdir/alacritty"
+[ ! -d "$desktopdir" ] && mkdir -p $desktopdir && echo "Made local desktop directory at $desktopdir"
+
+
+# Copy over the configuration directories to their new homes
 echo "Copying sway directory to $configdir..."
 cp -r sway $configdir
+
 echo "Copying waybar directory to $configdir..."
 cp -r waybar $configdir
+
+echo "Copying firefox.desktop to $desktopdir..."
+cp -r desktop-files $desktopdir
+
 echo "Copying SFMono font directory to $fontdir..."
 cp -r SFMono $fontdir
+
 echo "Copying SFUIDisplay font directory to $fontdir..."
 cp -r SFUIDisplay $fontdir
+
 echo "Copying SFUIText font directory to $fontdir..."
 cp -r SFUIText $fontdir
+
 echo "Copying .zshrc file to $homedir..."
 cp .zshrc $homedir/.zshrc
+
 echo "Done!"
